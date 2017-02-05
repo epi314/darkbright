e67699a9c78c535e24083cf1300ee37eccca0388
Pierre Enriquez <pierre@getperx.com>
2017-02-05 22:50:23 +0800

    Added common file for constants

---
+++ darkbright/Consts.swift
@@ -0,0 +1,12 @@
+//
+//  Consts.swift
+//  darkbright
+//
+//  Created by Pierre Enriquez on 5/2/17.
+//  Copyright © 2017 Three One Four. All rights reserved.
+//
+
+let BASE_URL = "https://api.darksky.net"
+let ENDPOINT = "forecast"
+let API_KEY = "deadbeef"
+let OPTIONALS = "exclude=minutely,hourly,alerts,flags&units=si"
